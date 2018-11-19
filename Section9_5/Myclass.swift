59ad8b7a1822a2f71b13b07ec178cbfd77375449
岩永 彩里 <>
2018-11-19 20:44:48 +0900

    p.209-p.228(Section9.4から9.5まで)

---
+++ Section9_5/Myclass.swift
@@ -0,0 +1,9 @@
+//
+//  Myclass.swift
+//  Section9_5
+//
+//  Created by 岩永 彩里 on 2018/11/19.
+//  Copyright © 2018年 岩永 彩里. All rights reserved.
+//
+
+import Foundation
---
+++ Section9_5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Section9_5
+//
+//  Created by 岩永 彩里 on 2018/11/19.
+//  Copyright © 2018年 岩永 彩里. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
